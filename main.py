0acf980beaee66db3428afa6e75b286bd606e507
Keita Sakurai <keitasakuraims@gmail.com>
2023-10-10 14:20:29 +0900

    Added to implement P and V using semaphores

--- main.py
+++ main.py
@@ -1,62 +1,54 @@
 import threading
-import time
 import random
+import time
+import sys
 
-# Constants
-n = 10  # Buffer size
-buffer = [0] * n  # Initialize buffer
+# 1. Experiment with different values of n, k, and t until a race condition is observed.
+
+buffer = [0] * 100
 next_in = 0
 next_out = 0
+count = 0
 
-# Semaphores
-mutex = threading.Semaphore(1)
-empty = threading.Semaphore(n)
-full = threading.Semaphore(0)
-
-# Producer thread
 def producer():
     global next_in
     while True:
-        k1 = random.randint(1, n)  # Burst size
-        time.sleep(random.uniform(0, 1))  # Sleep for a random duration
-        empty.acquire()
-        mutex.acquire()
+        k1 = random.randint(1, len(buffer) // 3)  # random k1
+        # write k1 data to buffer, regardless if buffer is full or not
         for i in range(k1):
-            buffer[(next_in + i) % n] = 1
-        next_in = (next_in + k1) % n
-        mutex.release()
-        full.release()
+            buffer[(next_in + i) % len(buffer)] += 1
+        next_in = (next_in + k1) % len(buffer)
+        time.sleep(random.random())  # random sleep time
 
-# Consumer thread
 def consumer():
-    global next_out
+    global next_out, count
     while True:
-        t2 = random.uniform(0, 1)  # Sleep for a random duration
-        time.sleep(t2)
-        k2 = random.randint(1, n)  # Burst size
-        mutex.acquire()
+        time.sleep(random.random())  # sleep random time interval
+        k2 = random.randint(1, len(buffer) // 3)  # random k2
         for i in range(k2):
-            data = buffer[(next_out + i) % n]
+            data = buffer[(next_out + i) % len(buffer)]
             if data > 1:
-                print("Race condition detected!: Consumer too slow.")
-                exit(1)
+                print("Race condition detected! Consumer too slow")
+                sys.exit(1)
             elif data == 0:
-                print("Race condition detected!: Producer too slow.")
-                exit(1)
+                print("Race condition detected! Producer too slow")
+                sys.exit(1)
             else:
-                buffer[(next_out + i) % n] = 0
-        next_out = (next_out + k2) % n
-        mutex.release()
-        empty.release()
+                buffer[(next_out + i) % len(buffer)] = 0  # clear buffer
+        next_out = (next_out + k2) % len(buffer)
+        count += 1
+        print(f"Round {count} no race condition detected yet")
 
-# Create producer and consumer threads
-producer_thread = threading.Thread(target=producer)
-consumer_thread = threading.Thread(target=consumer)
+# Create producer thread
+t1 = threading.Thread(target=producer)
 
-# Start the threads
-producer_thread.start()
-consumer_thread.start()
+# Create consumer thread
+t2 = threading.Thread(target=consumer)
 
-# Wait for threads to finish (you can add a termination condition)
-producer_thread.join()
-consumer_thread.join()
+# Start both threads
+t1.start()
+t2.start()
+
+# Join both threads
+t1.join()
+t2.join()
